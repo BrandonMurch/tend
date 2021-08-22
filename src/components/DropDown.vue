984f5b9559ad7080ce0ca1727a7f0ff7b2cd5d4c
Brandon Murch <brandon.murch@protonmail.com>
2021-08-22 16:52:57 +1000

    |WIP| Fix animation for drop down
    Still needs to monitor children for changes in size.

--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -1,16 +1,45 @@
 <template>
-	<div :class="{ open: isOpen, container: true }">
-		<!-- Named slots, in the future would like to add more parts to the drop down
-         https://vuejs.org/v2/guide/components-slots.html#Named-Slots-->
-		<slot name="inside" />
+	<div
+		:class="{ open: isOpen, container: true }"
+		ref="container"
+		:style="{ height: height }"
+	>
+		<div ref="contents">
+			<!-- Named slots, in the future would like to add more parts to the drop down
+			https://vuejs.org/v2/guide/components-slots.html#Named-Slots-->
+			<slot name="inside" />
+		</div>
 	</div>
 </template>
 
 <script>
+import { onUpdated, ref } from "vue";
+
 export default {
 	name: "DropDown",
 	props: {
 		isOpen: Boolean,
+	},
+	setup(props) {
+		const contents = ref(null);
+		const container = ref(null);
+		let height = ref(0);
+
+		const updateHeight = () => {
+			container.value.style.height = props.isOpen
+				? contents.value.clientHeight + "px"
+				: 0;
+		};
+
+		onUpdated(() => {
+			updateHeight();
+		});
+
+		return {
+			contents,
+			container,
+			height,
+		};
 	},
 };
 </script>
@@ -20,20 +49,20 @@ export default {
 	position: relative;
 	left: -10%;
 	width: 120%;
-	max-height: 0;
 	/* https://www.w3schools.com/css/css3_transitions.asp */
-	transition: 1s ease-out;
-	transition-property: max-height;
+	transition: 1.5s ease-in-out;
+	transition-property: height;
 	box-shadow: inset 0px 0px 4px 4px grey;
 	overflow: hidden;
 }
 
 .open {
-	max-height: 500px;
 	/* Uneven transition times due to the max-height css animation work around. */
-	transition: 1s ease-in;
-	transition-property: max-height;
+	transition: 1.5s ease-in-out;
+	transition-property: height;
 	border-top: 1px solid black;
 	border-bottom: 1px solid black;
 }
 </style>
+
+// https://markus.oberlehner.net/blog/refs-and-the-vue-3-composition-api/
